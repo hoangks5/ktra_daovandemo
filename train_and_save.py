d6f7c934168b2294e6e1d448e0f3ad75f5949950
Nguyễn Đức Hoàng <81577195+hoangks5@users.noreply.github.com>
2021-10-13 22:16:35 +0700

    Add files via upload

--- train_and_save.py
+++ train_and_save.py
@@ -1,126 +1,138 @@
-import docx
-import re
 import nltk
-
-nltk.download('punkt')
-from nltk.tokenize import sent_tokenize
-import json
 from nltk.stem.lancaster import LancasterStemmer
+from nltk.util import pr
 stemmer = LancasterStemmer()
-from tensorflow.python.framework import ops
-import numpy as np
+import pickle
+import numpy
 import tflearn
 import tensorflow
 import random
-import pickle
-
-###################################################################
-
-with open('main.json') as json_data:
-    intents = json.load(json_data)
-
-words = []
-classes = []
-documents = []
-stop_words = ['?', 'a', 'an', 'the']
-
-for intent in intents['intents']:
-    for pattern in intent['text']:
-
-        w = nltk.word_tokenize(pattern)
-        words.extend(w)
-        documents.append((w, intent['title']))
-        if intent['title'] not in classes:
-            classes.append(intent['title'])
-
-words = [stemmer.stem(w.lower()) for w in words if w not in stop_words]
-words = sorted(list(set(words)))
-classes = sorted(list(set(classes)))
-
-training = []
-output = []
-output_empty = [0] * len(classes)
-
-# training set, bag of words for each sentence
-for doc in documents:
-    bag = []
-    pattern_words = doc[0]
-    pattern_words = [stemmer.stem(word.lower()) for word in pattern_words]
-
-    for w in words:
-        bag.append(1) if w in pattern_words else bag.append(0)
-
-    # output is a '0' for each tag and '1' for current tag
-    output_row = list(output_empty)
-    output_row[classes.index(doc[1])] = 1
-
-    training.append([bag, output_row])
-random.shuffle(training)
-training = np.array(training)
-
-# create train and test lists
-train_x = list(training[:,0])
-train_y = list(training[:,1])
-
-ops.reset_default_graph()
-# Build neural network
-net = tflearn.input_data(shape=[None, len(train_x[0])])
-net = tflearn.fully_connected(net, 8)
-net = tflearn.fully_connected(net, 8)
-net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
-net = tflearn.regression(net, optimizer='adam', loss='categorical_crossentropy')
-
-# Define model and setup tensorboard
-model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
-# Start training
-#model.fit(train_x, train_y, n_epoch=300, batch_size=8, show_metric=True)
-#model.save('model.tflearn')
-
-pickle.dump( {'words':words, 'classes':classes, 'train_x':train_x, 'train_y':train_y}, open( "training_data", "wb" ) )
-
-# restore our data structures
-import pickle
-data = pickle.load( open( "training_data", "rb" ) )
-words = data['words']
-classes = data['classes']
-train_x = data['train_x']
-train_y = data['train_y']
-
-# import intents file
 import json
-with open('main.json') as json_data:
-    intents = json.load(json_data)
+import os
+import itertools
+from math import log
+import re
+from tensorflow.python.framework import ops
+import re
+import json
+import itertools
+from math import log
+import datetime
+import webbrowser as wb
 
-model.load('./model.tflearn')
 
-def clean_up_sentence(sentence):
-    sentence_words = nltk.word_tokenize(sentence)
-    sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
-    return sentence_words
+        
 
-# bag of words
-def bow(sentence, words, show_details=False):
-    sentence_words = clean_up_sentence(sentence)
-    # bag of words
-    bag = [0]*len(words)  
-    for s in sentence_words:
+
+with open("training.json",encoding="utf8") as file:
+    data = json.load(file)
+try:
+    with open("data.pickle", "ab") as f:
+        words, labels, training, output = pickle.load(f)
+except:
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
+    
+    for intent in data["intents"]:
+        for pattern in intent["text"]:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["title"])
+            
+            if intent["title"] not in labels:
+                labels.append(intent["title"])
+                
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+    words = sorted(list(set(words)))
+    
+    labels = sorted(labels)
+    
+    training = []
+    output = []
+    
+    out_empty = [0 for _ in range(len(labels))]
+    
+    for x, doc in enumerate(docs_x):
+        bag = []
+        
+        wrds = [stemmer.stem(w) for w in doc]
+        
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
+                
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
+        
+        training.append(bag)
+        output.append(output_row)
+        
+        
+    training = numpy.array(training)
+    output = numpy.array(output)
+    
+    with open("data.pickle", "ab") as f:
+        pickle.dump((words, labels, training, output), f)
+ops.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+
+try:
+    model.load(model.tflearn)
+except:
+    pass
+    model.fit(training, output, n_epoch=100, batch_size=8, show_metric=True)
+    model.save("model.tflearn")
+    
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+    
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+    
+    for se in s_words:
         for i,w in enumerate(words):
-            if w == s: 
+            if w == se:
                 bag[i] = 1
-                if show_details:
-                    print ("found in bag: %s" % w)
+    return numpy.array(bag)
 
-    return(np.array(bag))
 
-# data structure to hold user context
-context = {}
-
-ERROR_THRESHOLD = 0.25
-def test(sentence):
-    results = model.predict([bow(sentence, words)])[0]
-    results = [[i,r] for i,r in enumerate(results) if r>ERROR_THRESHOLD]
-    results.sort(key=lambda x: x[1], reverse=True)
-    return_list = []
-    for r in results:
-        return_list.append((classes[r[0]], r[1]))
-    return return_list
+ 
+    
+    
+def chat():
+    f = open('ss.txt','r',encoding='utf-8')
+    f = f.read()
+    f = f.split('.')
+    N = []
+    M = []
+    Q = []
+    for inp in f:
+        results = model.predict([bag_of_words(inp, words)])[0]
+        results_index = numpy.argmax(results)
+        tag = labels[results_index]
+        if results[results_index] > 0.5:
+            for tg in data["intents"]:
+                if tg['title'] == tag:
+                    responses = tg['title']
+                    M.append(responses)
+    for iss in M:
+        kq = iss+' : '+str(M.count(iss)/len(M)*100)+' %'
+        N.append(kq)
+        for kq in N:
+            if kq not in Q:
+                Q.append(kq)
+    print(Q)
+chat()
